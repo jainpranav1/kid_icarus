6eb187c0491670093fe472e5b85d39525eaad8a0
jainpranav1 <pranavji@tamu.edu>
2022-09-05 22:58:41 -0500

    fixed file names

--- is_prime_gates/is_prime_gates.v
+++ is_prime_gates/is_prime_gates.v
@@ -12,6 +12,11 @@ module is_prime_gates(
     output P
   );
 
-  assign P = (~c & b) | (c & a);
+  wire d, e, f;
+
+  not not1(d,c);
+  and and1(e,d,b);
+  and and2(f,c,a);
+  or or1(P,e,f);
 
 endmodule
--- is_prime_rtl.v
+++ is_prime_rtl.v
@@ -12,11 +12,6 @@ module is_prime_rtl(
     output P
   );
 
-  wire d, e, f;
-
-  not not1(d,c);
-  and and1(e,d,b);
-  and and2(f,c,a);
-  or or1(P,e,f);
+  assign P = (~c & b) | (c & a);
 
 endmodule
